f0da60fd16b6f5d7dc62df353e8c450478934023
Mike Sammartino <sammartino.mike@gmail.com>
2025-07-09 15:38:38 -0500

    style: layout FooterNav with flexbox

--- components/FooterNav.vue
+++ components/FooterNav.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
-    social(s)
-  </div>
-  <div>
-    copywrite
-  </div>
+  <UContainer class="flex flex-col">
+    <div id="social" class="flex justify-center">
+      <UButton
+        to="https://www.instagram.com/river.shepard/"
+        icon="lucide:instagram"
+      >
+        @river.shepard
+      </UButton>
+    </div>
+    <div id="copyright" class="flex justify-center">
+      <p><UIcon name="lucide:copyright" /> River Shepard Photographic Arts</p>
+    </div>
+  </UContainer>
 </template>
