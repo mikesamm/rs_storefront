856eb429f97ef054dd34f329c11bf24ecd6bec93
Mike Sammartino <sammartino.mike@gmail.com>
2025-07-09 15:24:25 -0500

    style: layout HeaderNav with flexbox

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
-    logo?
-  </div>
-  <div>
-    hamburger
-  </div>
-  <div>
-    River Shepard Photographic Arts
-  </div>
+  <UContainer class="flex flex-col">
+    <UContainer id="nav-header" class="flex flex-row justify-between">
+      <div>logo</div>
+      <div>
+        <USlideover close-icon="lucide:x">
+          <UButton icon="lucide:align-justify" />
+          <template #body> Menu goes here </template>
+        </USlideover>
+      </div>
+    </UContainer>
+    <div id="banner">
+      <h1>River Shepard Photographic Arts</h1>
+    </div>
+  </UContainer>
 </template>
